042a436c1d8510b406f4b82535bc32a46ff3e55e
Wrdlprmpf <58952423+Wrdlprmpf@users.noreply.github.com>
2020-10-20 13:31:00 +0200

    method implemented

---
+++ Aufgabenblatt0/src/main/java/Einstiegsbeispiel.java
@@ -0,0 +1,36 @@
+//Wetscher Nicolas 11845621
+
+import java.util.HashMap;
+
+public class Einstiegsbeispiel {
+	public static void main(String[] args) {
+		
+		int[] array = new int[]{1,1,1,2,2,2,2,3,4,4,4};
+
+		System.out.println(getRepetitions(array).values());
+
+		HashMap<Integer,Integer> exm = getRepetitions(array);
+
+
+		for (Integer ex : exm.keySet()) {
+			System.out.println(ex);
+
+		}
+	}
+
+	public static HashMap getRepetitions(int[] arr){
+
+		HashMap<Integer, Integer> reps = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < arr.length ; i++) {
+			if(reps.containsKey(arr[i])){
+				reps.put(arr[i],reps.get(arr[i])+1);
+			}
+			else{
+				reps.put(arr[i],1);
+			}
+		}
+		return reps;
+	}
+
+}
